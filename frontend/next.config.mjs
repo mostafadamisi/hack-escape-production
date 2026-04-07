e2271a8b7137fdbc8ac7dfae4cf302e6e53bdc6f
mostafadamisi <mostafadamisi@gmail.com>
2026-04-07 17:15:03 +0300

    fix: Optimized image serving and proxy resolution

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5001/api/:path*',
+        destination: 'http://localhost:5001/api/:path*',
       },
       {
         source: '/uploads/:path*',
-        destination: 'http://127.0.0.1:5001/uploads/:path*',
+        destination: 'http://localhost:5001/uploads/:path*',
       },
     ];
   },
