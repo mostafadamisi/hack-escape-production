5651a281902d34b3f49312e1f376dd43ebeaf663
mostafadamisi <mostafadamisi@gmail.com>
2026-04-08 23:07:39 +0300

    fix: Remove stale /test-email route causing startup crash

--- backend/src/routes/publicRoutes.js
+++ backend/src/routes/publicRoutes.js
@@ -11,7 +11,6 @@ router.get('/gallery', publicController.getGallery);
 router.get('/media', publicController.getMedia);
 router.get('/terminal', publicController.getTerminal);
 router.post('/inquiries', publicController.createInquiry);
-router.get('/test-email', publicController.testEmail);
 router.get('/test-telegram', publicController.testTelegram);
 
 module.exports = router;
